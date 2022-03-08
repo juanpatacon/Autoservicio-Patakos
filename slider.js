9684566becce20f1bbcba1b09985986603c7b7aa
juan carlos patacon parada <jpataconparada@gmail.com>
2022-03-08 18:03:13 -0500

    pruebas de variables

--- slider.js
+++ slider.js
@@ -1,1 +1,27 @@
-console.log('Autoservicio Patakos hola mundo')
+    // variables
+
+     var nombre = ("Montserrat");
+         nombre = "Montserrat";
+
+
+
+
+
+
+
+console.log('hello world')
+
+  var Saldo = 0;
+     
+       while(Saldo < 50) {
+
+       console.log(Saldo);
+       Saldo++; 
+      
+      }
+
+      function wellcome() {
+        console.log('Congratulation')
+      }
+
+      wellcome();
